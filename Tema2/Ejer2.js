cd03f44347d7eca3a1728c31c2c37287559f5a23
Changfeng Zheng <czhe581@g.educaand.es>
2022-09-29 14:33:01 +0200

    Mod3

--- Tema2/Ejer2.js
+++ Tema2/Ejer2.js
@@ -11,13 +11,16 @@ console.log("Se han escrito las potencias de 2 menores de 1000");
 
 /*Utilizado while */
 let aux2 = 1;
-let x = 0;
+let x = 2;
 
-while(aux2 <= 1000){
-    x *= 2
+while(x <= 1000){
     console.log("2 elevado a " + aux2 + " es igual a " + x);
+    x *= 2
     aux2++;
 }
 console.log("Se han escrito las potencias de 2 menores de 1000");
 
 /*Utilizado do while */
+
+
+
