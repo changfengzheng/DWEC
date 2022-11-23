20df19f30e6f8ebac8af5e399edc8e1b9dc02897
Changfeng Zheng <czhe581@g.educaand.es>
2022-11-23 13:28:14 +0100

    upd

--- Tema4/F5_Changfeng_Zheng/Ejer2/index.js
+++ Tema4/F5_Changfeng_Zheng/Ejer2/index.js
@@ -1,16 +1,16 @@
 //Al pulsar botón del raton se pone el amarillo
 function Amarillo() {
-    document.body.style.backgroundColor = "#FFFF00";
+    document.getElementById("cuadroTxt").style.backgroundColor = "#FFFF00";
 
 }
 //Al mover el cursor se pone el blanco
 function Blanco() {
-    document.body.style.backgroundColor = "White";
+    document.getElementById("cuadroTxt").style.backgroundColor = "White";
 
 }
 //Al pulsar una tecla se pone en azul
 function Azul() {
-    document.body.style.backgroundColor = "#CCE6FF";
+    document.getElementById("cuadroTxt").style.backgroundColor = "#CCE6FF";
 
 }
 
